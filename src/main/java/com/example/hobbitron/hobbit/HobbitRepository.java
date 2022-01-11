3574d5c841aba45956ed01cb88a092f36e54a4cd
lukasze <wyspianski.lukasz@gmail.com>
2022-01-11 21:32:23 +0100

    Dodanie opisów w repozytorium

--- src/main/java/com/example/hobbitron/hobbit/HobbitRepository.java
+++ src/main/java/com/example/hobbitron/hobbit/HobbitRepository.java
@@ -17,15 +17,24 @@ import java.util.List;
  */
 public interface HobbitRepository extends JpaRepository<Hobbit, Long>, CustomHobbitRepository {
 
+    /*
+        Nazwa metody nie jest trywialna - na jej podstawie, Spring Data wygeneruje zapytanie
+        https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+     */
     Hobbit findByFirstNameAndLastName(String firstName, String lastName);
-
+    /*
+        JPQL: Hobbit to nazwa encji, nie tabeli!
+        Jeśli używamy @Query, nazwa metody nie ma zanczenia dla generowanego zapytania
+     */
     @Query("SELECT h FROM Hobbit h WHERE h.firstName=:firstName AND h.lastName=:lastName ")
     Hobbit findHobbitUsingQuery(String firstName, String lastName);
 
     @Query(
             nativeQuery = true,
+            // SQL - hobbits to nazwa tabeli
             value = "select * from hobbits where hobbits.first_name = :firstName and hobbits.last_name = :lastName")
     Hobbit findHobbitUsingNativeQuery(String firstName, String lastName);
 
+    // findUsingNamedQuery: zapytanie zdefiniowane w encji Hobbit (@NamedQuery)
     List<Hobbit> findUsingNamedQuery(String firstName, String lastName);
 }
